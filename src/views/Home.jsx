d15acd7e33a219dcf34578aeed4f536d9856887a
Jay Cortes <cortesjay1991@gmail.com>
2025-07-17 16:31:59 +0200

    layout has been updated

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import '../assets/styles/Home.css'
+import '../App.css';
 
 const Home = () => {
   return (
     <div className='main-container'>
+      <div className='layout'>
 
+      </div>
     </div>
   )
 }

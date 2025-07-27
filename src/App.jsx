3c807298f6ac0cb2bdd1dee82597a6b08429b13b
Jay Cortes <cortesjay1991@gmail.com>
2025-07-27 23:49:19 +0200

    Appointmnets has been created

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './views/Home'
 import About from './views/About'
 import Contact from './views/Contact'
+import Appointments from './views/Appointments';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/appointment" element={<Appointments />} />
         </Routes>
       </div>
     </Router>
